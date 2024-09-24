6baf5cdd6ef321a2681ee87f5b0a85511f7ae5db
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-09-24 22:09:20 +0900

    init project

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,5 @@
+mod socks;
+
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 use tokio_stream::wrappers::ReceiverStream;
@@ -5,41 +7,52 @@ use tonic::Request;
 use tonic::transport::Channel;
 use proto::runnel::tunnel_client::TunnelClient;
 use proto::runnel::TunnelRequest;
-use anyhow::{anyhow, Result};
-
-async fn handle_client(client: &mut TunnelClient<Channel>) {
-    let mut last_seen_pong: u32 = 0;
-    let (tx, rx) = mpsc::channel(10000);
-    let ack = ReceiverStream::new(rx);
-
-    let response = client.tunnel_message(Request::new(ack)).await.unwrap();
-
-    let message = format!("last seen pong: {}", last_seen_pong);
-    // kick start the pingpong with an init tx.send
-    tx.send(TunnelRequest {
-        message: message.into_bytes(),
-        over: false,
-        target: "127.0.2.1".into(),
-    }).await.unwrap();
-    let mut stream = response.into_inner();
-    while let Some(result) = stream.next().await {
-        let result = result.unwrap();
-        println!("Message recieved: {:?}", String::from_utf8(result.data));
-        let message = format!("last seen pong: {}", last_seen_pong);
-        tx.send(TunnelRequest {
-            message: message.into_bytes(),
-            over: false,
-            target: "127.0.0.1".into(),
-        }).await.unwrap();
-    }
-}
+use anyhow::Result;
+use tokio::net::TcpListener;
+// let mut client = TunnelClient::connect("http://127.0.0.1:50051")
+// .await.unwrap();
+//
+// handle_client(&mut client).await;
+// async fn handle_client(client: &mut TunnelClient<Channel>) {
+//     let mut last_seen_pong: u32 = 0;
+//     let (tx, rx) = mpsc::channel(10000);
+//     let ack = ReceiverStream::new(rx);
+//
+//     let response = client.tunnel_message(Request::new(ack)).await.unwrap();
+//
+//     let message = format!("last seen pong: {}", last_seen_pong);
+//     // kick start the pingpong with an init tx.send
+//     tx.send(TunnelRequest {
+//         message: message.into_bytes(),
+//         over: false,
+//         target: "127.0.2.1".into(),
+//     }).await.unwrap();
+//     let mut stream = response.into_inner();
+//     while let Some(result) = stream.next().await {
+//         let result = result.unwrap();
+//         println!("Message recieved: {:?}", String::from_utf8(result.data));
+//         let message = format!("last seen pong: {}", last_seen_pong);
+//         tx.send(TunnelRequest {
+//             message: message.into_bytes(),
+//             over: false,
+//             target: "127.0.0.1".into(),
+//         }).await.unwrap();
+//     }
+// }
 
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = TunnelClient::connect("http://127.0.0.1:50051")
-        .await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:6080").await?;
+    println!("SOCKS5 server listening on 0.0.0.0:6080");
 
-    handle_client(&mut client).await;
-    Ok(())
+    loop {
+        let (socket, addr) = listener.accept().await?;
+        println!("New connection from {}", addr);
+        tokio::spawn(async move {
+            if let Err(e) = socks::handle_socks5_server(socket).await {
+                eprintln!("Error handling client {}: {:?}", addr, e);
+            }
+        });
+    }
 }
--- agent/src/socks.rs
+++ agent/src/socks.rs
@@ -1,53 +1,37 @@
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpStream;
+use anyhow::{Result, anyhow};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use std::error::Error;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("0.0.0.0:6080").await?;
-    println!("SOCKS5 server listening on 0.0.0.0:6080");
-
-    loop {
-        let (socket, addr) = listener.accept().await?;
-        println!("New connection from {}", addr);
-        tokio::spawn(async move {
-            if let Err(e) = handle_client(socket).await {
-                eprintln!("Error handling client {}: {:?}", addr, e);
-            }
-        });
-    }
-}
-
-async fn handle_client(mut client: TcpStream) -> Result<(), Box<dyn Error>> {
-    // 1. 握手阶段
+pub(crate) async fn handle_socks5_server(mut client: TcpStream) -> Result<()> {
+    // 1. Handshake phase
     let mut buf = [0u8; 2];
     client.read_exact(&mut buf).await?;
     if buf[0] != 0x05 {
-        return Err("Unsupported SOCKS version".into());
+        return Err(anyhow!("Unsupported SOCKS version"));
     }
     let n_methods = buf[1] as usize;
     let mut methods = vec![0u8; n_methods];
     client.read_exact(&mut methods).await?;
-    // 只支持不认证
+    // Only supports no authentication
     if !methods.contains(&0x00) {
-        // 回复不支持的认证方法
+        // Reply with unsupported authentication method
         client.write_all(&[0x05, 0xFF]).await?;
-        return Err("No acceptable authentication methods".into());
+        return Err(anyhow!("No acceptable authentication methods"));
     }
-    // 回复选择不认证
+    // Reply with no authentication selected
     client.write_all(&[0x05, 0x00]).await?;
 
-    // 2. 请求阶段
+    // 2. Request phase
     let mut header = [0u8; 4];
     client.read_exact(&mut header).await?;
     if header[0] != 0x05 {
-        return Err("Unsupported SOCKS version in request".into());
+        return Err(anyhow!("Unsupported SOCKS version in request"));
     }
     let cmd = header[1];
     let _reserved = header[2];
     let addr_type = header[3];
 
-    // 解析目标地址
+    // Parse destination address
     let dest_addr = match addr_type {
         0x01 => { // IPv4
             let mut addr = [0u8; 4];
@@ -55,7 +39,7 @@ async fn handle_client(mut client: TcpStream) -> Result<(), Box<dyn Error>> {
             let ip = std::net::Ipv4Addr::from(addr);
             ip.to_string()
         }
-        0x03 => { // 域名
+        0x03 => { // Domain name
             let mut len = [0u8; 1];
             client.read_exact(&mut len).await?;
             let mut addr = vec![0u8; len[0] as usize];
@@ -68,61 +52,61 @@ async fn handle_client(mut client: TcpStream) -> Result<(), Box<dyn Error>> {
             let ip = std::net::Ipv6Addr::from(addr);
             ip.to_string()
         }
-        _ => return Err("Unsupported address type".into()),
+        _ => return Err(anyhow!("Unsupported address type")),
     };
 
-    // 解析目标端口
+    // Parse destination port
     let mut port_bytes = [0u8; 2];
     client.read_exact(&mut port_bytes).await?;
     let port = u16::from_be_bytes(port_bytes);
 
     println!("Request to connect to {}:{}", dest_addr, port);
 
-    // 只支持 CONNECT 命令
+    // Only supports CONNECT command
     if cmd != 0x01 {
-        send_reply(&mut client, 0x07, &"0.0.0.0", 0).await?;
-        return Err("Command not supported".into());
+        send_reply(&mut client, 0x07, "0.0.0.0", 0).await?;
+        return Err(anyhow!("Command not supported"));
     }
 
-    // 尝试建立目标连接
+    // Attempt to establish connection to the target
     let target = format!("{}:{}", dest_addr, port);
     match TcpStream::connect(target).await {
         Ok(mut target_stream) => {
-            // 获取本地地址信息用于回复客户端
+            // Get local address information for replying to the client
             let local_addr = target_stream.local_addr()?;
             send_reply(&mut client, 0x00, &local_addr.ip().to_string(), local_addr.port()).await?;
 
-            // 3. 数据转发
+            // 3. Data forwarding
             let (mut ri, mut wi) = client.split();
             let (mut ro, mut wo) = target_stream.split();
 
-            // 双向转发
+            // Bidirectional forwarding
             let client_to_target = tokio::io::copy(&mut ri, &mut wo);
             let target_to_client = tokio::io::copy(&mut ro, &mut wi);
 
             tokio::try_join!(client_to_target, target_to_client)?;
         }
         Err(_) => {
-            // 连接失败
-            send_reply(&mut client, 0x05, &"0.0.0.0", 0).await?;
-            return Err("Failed to connect to target".into());
+            // Connection failed
+            send_reply(&mut client, 0x05, "0.0.0.0", 0).await?;
+            return Err(anyhow!("Failed to connect to target"));
         }
     }
 
     Ok(())
 }
 
-async fn send_reply(client: &mut TcpStream, rep: u8, bind_addr: &str, bind_port: u16) -> Result<(), Box<dyn Error>> {
+async fn send_reply(client: &mut TcpStream, rep: u8, bind_addr: &str, bind_port: u16) -> Result<()> {
     let mut reply = vec![0x05, rep, 0x00];
 
-    // 简单处理IPv4
+    // Handle IPv4 and IPv6 addresses
     if let Ok(ip) = bind_addr.parse::<std::net::Ipv4Addr>() {
         reply.push(0x01);
         reply.extend_from_slice(&ip.octets());
     } else if let Ok(ip) = bind_addr.parse::<std::net::Ipv6Addr>() {
         reply.push(0x04);
         reply.extend_from_slice(&ip.octets());
-    } else { // 域名
+    } else { // Domain name
         let addr = bind_addr.as_bytes();
         reply.push(0x03);
         reply.push(addr.len() as u8);
